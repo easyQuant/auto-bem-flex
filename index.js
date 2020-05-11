2e2384dc90a3e50c768e3f14523bf268cc18662e
tianhao <trade_tianhao@163.com>
2020-05-11 11:59:44 +0800

    增加全局命令

--- index.js
+++ index.js
@@ -6,28 +6,30 @@ const Spinner = CLI.Spinner
 const files = require('./lib/files');
 const render = require('./lib/render')
 
-// clear();
-console.log(
-  chalk.green(
-    figlet.textSync('Auto BEM Flex', { horizontalLayout: 'full' })
-  )
-);
-
-console.log(chalk.green('快速创建符合bem-flex 规范的小程序组件和页面'));
-
-const inquirer = require('./lib/inquirer');
-
-const run = async (cb) => {
-  const credentials = await inquirer.askGithubCredentials();
-  cb && cb(credentials)
+function init () {
+  console.log(
+    chalk.green(
+      figlet.textSync('Auto BEM Flex', { horizontalLayout: 'full' })
+    )
+  );
+  
+  console.log(chalk.green('快速创建符合bem-flex 规范的小程序组件和页面'));
+  
+  const inquirer = require('./lib/inquirer');
+  
+  const run = async (cb) => {
+    const credentials = await inquirer.askGithubCredentials();
+    cb && cb(credentials)
+  }
+  
+  run((credentials) => {
+      console.log(`当前路径 ${ chalk(files.getCurrentDirectoryBase()) }`)
+      const status = new Spinner('正在创建对应布局的组件中 ...');
+      status.start();
+      render(credentials)
+      status.stop()
+      console.log(chalk.green('✅ 组件创建完成'));
+  });
 }
 
-run((credentials) => {
-    console.log(`当前路径 ${ chalk(files.getCurrentDirectoryBase()) }`)
-    const status = new Spinner('正在创建对应布局的组件中 ...');
-    status.start();
-    render(credentials)
-    status.stop()
-    console.log(chalk.green('✅ 组件创建完成'));
-});
-
+module.exports = { init }
